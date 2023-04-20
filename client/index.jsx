a21c558993c674bb40228b0ba78cebf6e0d7cfcb
simple-sifu <kattyntommy@gmail.com>
2023-04-20 17:17:30 -0400

    refactor

--- client/index.jsx
+++ client/index.jsx
@@ -1,33 +1,6 @@
-import "./styles.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import createStore from "./Core/CreateStore";
-import HttpGateway from "./Core/HttpGateway";
-import StatsComponent from "./Stats/StatsComponent";
-import AddBookComponent from "./Books/AddBooks/AddBookComponent";
-import BookListComponent from "./Books/BookList/BookListComponent";
-
-function App() {
-  const httpGateway = new HttpGateway();
-  const store = createStore(httpGateway);
-
-  return (
-    <Provider store={store}>
-      <div className="flex-row-container">
-        <div className="flex-row-item">
-          <AddBookComponent />
-        </div>
-        <div className="flex-row-item">
-          <StatsComponent />
-        </div>
-        <div className="flex-row-item">
-          <BookListComponent />
-        </div>
-      </div>
-    </Provider>
-  );
-}
+import App from './App';
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
